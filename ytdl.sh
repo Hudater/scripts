a3058cbc3cc0f64064e25b4bf0dc2145a0f17027
Harshit Mani Tripathi <vandannaamhaimera@gmail.com>
2021-01-20 10:42:43 +0530

    boii

---
+++ ytdl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#cd /mnt/Media/Audios/Music
+youtube-dl -f 140 -i -o "/mnt/Media/Audios/Music" "https://www.youtube.com/playlist?list=PLIm43Ze1XVTU4db599A7jN715sFcoqGTR"
+
+#"https://www.youtube.com/playlist?list=PLIm43Ze1XVTU4db599A7jN715sFcoqGTR"
+#%(title)s
