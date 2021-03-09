25828c475e3a916035b4790bd0901c2e7b8ea4af
Harshit Mani Tripathi <vandannaamhaimera@gmail.com>
2021-03-09 22:03:20 +0530

    kldfaskl;f

--- ytmusic.sh
+++ ytmusic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#cd /mnt/Media/Audios/Music
+#cd /mnt/Media/Music
 youtube-dl -f 140 -i -o "/mnt/Media//Music/%(title)s-%(id)s.%(ext)s" "https://www.youtube.com/playlist?list=PLIm43Ze1XVTU4db599A7jN715sFcoqGTR"
 
 #"https://www.youtube.com/playlist?list=PLIm43Ze1XVTU4db599A7jN715sFcoqGTR"
