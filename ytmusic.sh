5990f256901f583a7c4d203c33e7f23e6901a2d5
Harshit Mani Tripathi <vandannaamhaimera@gmail.com>
2021-09-27 03:24:54 +0530

    something changed

--- ytmusic.sh
+++ ytmusic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #cd /mnt/Media/Music
-youtube-dl -f 140 -i -o "/mnt/Media//Music/%(title)s-%(id)s.%(ext)s" "https://www.youtube.com/playlist?list=PLIm43Ze1XVTU4db599A7jN715sFcoqGTR"
+youtube-dl -f 140 -i --add-metadata -o "/mnt/Media//Music/%(title)s-%(id)s.%(ext)s" "https://www.youtube.com/playlist?list=PLIm43Ze1XVTU4db599A7jN715sFcoqGTR"
 
 #"https://www.youtube.com/playlist?list=PLIm43Ze1XVTU4db599A7jN715sFcoqGTR"
 #%(title)s
